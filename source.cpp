30dae96aa5a8cb74d173f3277023c154bd489ec6
ngdinhtai999 <dinhtai147@gmail.com>
2020-10-06 11:21:19 +0700

    tai

--- source.cpp
+++ source.cpp
@@ -1,14 +1,36 @@
 int USCLN(int a, int b)
 {
+	a=10;
+	b=20;
+	if(a==0&&b==0){
+		cout<<"1"<<endl;
+		}
+	else if(a!=0 && b==0)
+	{
+		cout<<"2"<<endl;
+	}
+	else if(a==0&&b!=0)
+		cout<<"3"<<endl;
+	return 0;
 }
 void hello()
 {
+	cout<<"Heloooooooo";
 }
-int tru(int a, int b)
+int tru(int a, int b){
+	cin>>a;
+	cin>>b;
+	return a-b;
 {
 int cong(int a, int b)
 {
+	a=10;
+	b=4;
+	return a+b;
 }
 int nhan(int a, int b)
 {
+	a=10;
+	b=4;
+	return a*b;
 }
